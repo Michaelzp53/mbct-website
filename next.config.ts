4ee849e6ba8e3a0192b54bde855a1ecb0180de44
MichaelZhou <Michaelzp53@users.noreply.github.com>
2026-04-25 21:18:08 +0800

    fix: remove www redirect for Baidu verification

--- next.config.ts
+++ next.config.ts
@@ -5,21 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.marvelbros.com',
-          },
-        ],
-        destination: 'https://marvelbros.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
