c9605ac2c7a1622da6a5e11ab34797d198ec7986
MichaelZhou <Michaelzp53@users.noreply.github.com>
2026-09-23 12:03:19 +0800

    Add September 23 hotel guest quality article

--- src/app/[lang]/lean/article/[slug]/articles-data.ts
+++ src/app/[lang]/lean/article/[slug]/articles-data.ts
@@ -12697,6 +12697,7 @@ import newArticles20260911 from './articles-2026-09-11.json';
 import newArticles20260917 from './articles-2026-09-17.json';
 import newArticles20260920 from './articles-2026-09-20.json';
 import newArticles20260922 from './articles-2026-09-22.json';
+import newArticles20260923 from './articles-2026-09-23.json';
 
 const allArticles = [...articlesData, ...(newArticles20250523 as ArticleData[]), ...(newArticles20250525 as ArticleData[]), ...(newArticles20250527 as ArticleData[]), ...(newArticles20250528 as ArticleData[]), ...(newArticles20250529 as ArticleData[]), ...(newArticles20250530 as ArticleData[]), ...(newArticles20250531 as ArticleData[]), ...(newArticles20260602 as ArticleData[]), ...(newArticles20260603 as ArticleData[]), ...(newArticles20260604 as ArticleData[]), ...(newArticles20260605 as ArticleData[]), ...(newArticles20260606 as ArticleData[]), ...(newArticles20260607 as ArticleData[]), ...(newArticles20260609 as ArticleData[]), ...(newArticles20260610 as ArticleData[]),
 ...(newArticles20260611 as ArticleData[]), ...(newArticles20260613 as ArticleData[]), ...(newArticles20260614 as ArticleData[]), ...(newArticles20260615 as ArticleData[]), ...(newArticles20260616 as ArticleData[]),
@@ -12727,7 +12728,7 @@ const allArticles = [...articlesData, ...(newArticles20250523 as ArticleData[]),
   ...(newArticles20260806 as ArticleData[]),
   ...(newArticles20260807 as ArticleData[]),
   ...(newArticles20260808 as ArticleData[]),
-...(newArticles20260809 as ArticleData[]), ...(newArticles20260810 as ArticleData[]), ...(newArticles20260812 as ArticleData[]), ...(newArticles20260813 as ArticleData[]), ...(newArticles20260814 as ArticleData[]), ...(newArticles20260817 as ArticleData[]), ...(newArticles20260826 as ArticleData[]), ...(newArticles20260827 as ArticleData[]), ...(newArticles20260828 as ArticleData[]), ...(newArticles20260829 as ArticleData[]), ...(newArticles20260830 as ArticleData[]), ...(newArticles20260902 as ArticleData[]), ...(newArticles20260904 as ArticleData[]), ...(newArticles20260911 as ArticleData[]), ...(newArticles20260917 as ArticleData[]), ...(newArticles20260920 as ArticleData[]), ...(newArticles20260922 as ArticleData[])];
+...(newArticles20260809 as ArticleData[]), ...(newArticles20260810 as ArticleData[]), ...(newArticles20260812 as ArticleData[]), ...(newArticles20260813 as ArticleData[]), ...(newArticles20260814 as ArticleData[]), ...(newArticles20260817 as ArticleData[]), ...(newArticles20260826 as ArticleData[]), ...(newArticles20260827 as ArticleData[]), ...(newArticles20260828 as ArticleData[]), ...(newArticles20260829 as ArticleData[]), ...(newArticles20260830 as ArticleData[]), ...(newArticles20260902 as ArticleData[]), ...(newArticles20260904 as ArticleData[]), ...(newArticles20260911 as ArticleData[]), ...(newArticles20260917 as ArticleData[]), ...(newArticles20260920 as ArticleData[]), ...(newArticles20260922 as ArticleData[]), ...(newArticles20260923 as ArticleData[])];
 export const allArticlesData = allArticles;
 
 export function getArticleBySlug(slug: string) {
